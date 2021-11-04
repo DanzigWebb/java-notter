74b9c315c3ed2e3a6ced098be772158391c36562
Alexandr <valverin95@gmail.com>
2021-11-05 00:48:04 +0300

    fix: Util: обработан случай, когда Iterable пуст

--- src/main/java/com/example/notter/util/Util.java
+++ src/main/java/com/example/notter/util/Util.java
@@ -1,11 +1,15 @@
 package com.example.notter.util;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class Util {
     public static <T> List<T> IterableToList(Iterable<T> iterable) {
+        if (iterable == null) {
+            return new ArrayList<>();
+        }
         return StreamSupport.stream(iterable.spliterator(), false)
                 .collect(Collectors.toList());
     }
