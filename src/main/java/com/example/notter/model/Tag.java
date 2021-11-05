afaa88ba3390010d675dc9ded7fd765fdcd8ddd6
Alexandr <valverin95@gmail.com>
2021-11-05 16:41:23 +0300

    refactor: Tag: убраны свойства создания/обновления

--- src/main/java/com/example/notter/model/Tag.java
+++ src/main/java/com/example/notter/model/Tag.java
@@ -17,8 +17,6 @@ public class Tag {
         t.setId(entity.getId());
         t.setName(entity.getName());
         t.setColor(entity.getColor());
-        t.setCreateAt(entity.getCreatedAt());
-        t.setUpdateAt(entity.getUpdatedAt());
 
         return t;
     }
@@ -45,22 +43,6 @@ public class Tag {
 
     public void setColor(String color) {
         this.color = color;
-    }
-
-    public LocalDateTime getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(LocalDateTime createAt) {
-        this.createAt = createAt;
-    }
-
-    public LocalDateTime getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(LocalDateTime updateAt) {
-        this.updateAt = updateAt;
     }
 
 }
