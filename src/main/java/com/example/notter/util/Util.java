54422d3ae7f080403e6d788216c3b2496ff74765
Alexandr <valverin95@gmail.com>
2021-11-20 15:33:35 +0300

    feat: [back] Util: обновлен метод для преобразования данных для АПИ

--- src/main/java/com/example/notter/util/Util.java
+++ src/main/java/com/example/notter/util/Util.java
@@ -2,15 +2,18 @@ package com.example.notter.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 public class Util {
-    public static <T> List<T> IterableToList(Iterable<T> iterable) {
-        if (iterable == null) {
+    public static <T, R> List<T>
+    getModel(List<R> list, Function<? super R, ? extends T> fn) {
+        if (list == null) {
             return new ArrayList<>();
         }
-        return StreamSupport.stream(iterable.spliterator(), false)
+
+        return list
+                .stream().map(fn)
                 .collect(Collectors.toList());
     }
 }
