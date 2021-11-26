0c7930fec6074930193b8d34fa07917894f36f75
Alexandr <valverin95@gmail.com>
2021-11-26 16:53:36 +0300

    fix: [ui] textAreaAuto: добавлена проверка на изменения из ngControl

--- ui/ui/src/app/shared/components/textarea-auto/textarea-auto.directive.ts
+++ ui/ui/src/app/shared/components/textarea-auto/textarea-auto.directive.ts
@@ -1,14 +1,17 @@
-import { Directive, ElementRef, EventEmitter, Output } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, EventEmitter, OnDestroy, Optional, Output, Self } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: '[appTextareaAuto]',
   host: {
     'class': 'textarea textarea-ghost w-full resize-none leading-5 min-h-0 overflow-y-hidden px-1 py-1',
     'rows': '1',
-    '(input)': 'onInput.emit(ref.value)'
+    '(input)': 'onInput.emit(ref.value)',
   }
 })
-export class TextareaAutoDirective {
+export class TextareaAutoDirective implements AfterViewInit, OnDestroy {
 
   get ref(): HTMLTextAreaElement {
     return this.elementRef.nativeElement;
@@ -16,8 +19,26 @@ export class TextareaAutoDirective {
 
   @Output() onInput = new EventEmitter<string>();
 
+  private destroy$ = new Subject();
+
   constructor(
     private elementRef: ElementRef,
+    @Optional() @Self() private control: NgControl,
   ) {
+  }
+
+  ngAfterViewInit() {
+    if (this.control) {
+      this.control.control?.valueChanges
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(data => {
+          this.onInput.emit(data);
+        });
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- ui/ui/src/app/shared/components/textarea-auto/textarea-auto/textarea-auto.component.ts
+++ ui/ui/src/app/shared/components/textarea-auto/textarea-auto/textarea-auto.component.ts
@@ -1,11 +1,11 @@
 import {
   Component,
-  OnInit,
   ChangeDetectionStrategy,
   ElementRef,
   ViewChild,
   AfterViewInit,
-  ContentChild, OnDestroy
+  ContentChild,
+  OnDestroy,
 } from '@angular/core';
 import { TextareaAutoDirective } from '@app/shared/components/textarea-auto/textarea-auto.directive';
 import { Subject } from 'rxjs';
@@ -16,12 +16,8 @@ import { takeUntil } from 'rxjs/operators';
   templateUrl: './textarea-auto.component.html',
   styleUrls: ['./textarea-auto.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: {
-    'class': 'textarea textarea-ghost w-full resize-none leading-5 min-h-0 overflow-y-hidden px-1 py-1',
-    'rows': '1',
-  }
 })
-export class TextareaAutoComponent implements OnInit, AfterViewInit, OnDestroy {
+export class TextareaAutoComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('textClone') cloneRef!: ElementRef;
   @ContentChild(TextareaAutoDirective) textareaAutoDirective!: TextareaAutoDirective;
@@ -39,30 +35,27 @@ export class TextareaAutoComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor() {
   }
 
-  ngOnInit(): void {
-  }
-
   ngAfterViewInit() {
     this.setInitialValue();
     this.updateHeight();
     this.onInputChange();
   }
 
-  onInputChange() {
+  private onInputChange() {
     this.textareaAutoDirective.onInput.pipe(
       takeUntil(this.destroy$)
     ).subscribe(value => {
       this.clone.innerHTML = value + 'l';
       this.updateHeight();
-    })
+    });
   }
 
-  updateHeight() {
+  private updateHeight() {
     const height = this.cloneRef.nativeElement.offsetHeight;
     this.textarea.style.height = height + 'px';
   }
 
-  setInitialValue() {
+  private setInitialValue() {
     const value = this.textarea.value;
     this.clone.innerHTML = value + 'l';
   }
