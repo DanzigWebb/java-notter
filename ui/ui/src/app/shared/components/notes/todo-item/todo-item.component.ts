7dfc65dfa98ff0188e643f4c2593e73796317c30
Alexandr <valverin95@gmail.com>
2021-11-28 00:15:16 +0300

    feat: [ui] todos: добавлен компонент todoItem

---
+++ ui/ui/src/app/shared/components/notes/todo-item/todo-item.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { TodoDto } from '@app/models';
+import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-todo-item',
+  exportAs: 'appTodoItem',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TodoItemComponent implements OnInit, OnDestroy {
+
+  @Input() todo: TodoDto | null = null;
+
+  @Output() onToggle = new EventEmitter<boolean>();
+  @Output() onDelete = new EventEmitter<TodoDto>();
+  @Output() onChange = new EventEmitter<string>();
+
+  control = new FormControl('');
+
+  private destroy$ = new Subject();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.control.setValue(this.todo?.title);
+  }
+
+  changeEmit(title: string) {
+    if (title.trim() !== this.todo?.title.trim()) {
+      this.onChange.emit(title);
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
