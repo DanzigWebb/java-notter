cecca2ea1b12f25d2c1ae92528dbd0c6c0245b3e
Alexandr <valverin95@gmail.com>
2021-12-14 00:07:34 +0300

    feat: [ui] загрузка данных по dashboards при старте приложения

--- ui/ui/src/app/app.service.ts
+++ ui/ui/src/app/app.service.ts
@@ -3,6 +3,7 @@ import { forkJoin, Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { GroupFacade } from '@app/store/group';
 import { TagFacade } from '@app/store/tag';
+import { DashboardFacade } from '@app/store/dashboard';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { TagFacade } from '@app/store/tag';
 export class AppService {
 
   constructor(
+    private dash: DashboardFacade,
     private groups: GroupFacade,
     private tags: TagFacade,
   ) {
@@ -18,6 +20,7 @@ export class AppService {
   init() {
     const takeOne = <T>(obs: Observable<T>) => obs.pipe(take(1));
     return forkJoin([
+        takeOne(this.dash.getAll()),
         takeOne(this.groups.getAll()),
         takeOne(this.tags.getAll()),
         takeOne(this.tags.getColors())
