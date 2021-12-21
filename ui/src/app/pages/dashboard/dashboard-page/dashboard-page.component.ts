0649d7d664c5dda2be41d23e648852fb0689c669
Alexandr <valverin95@gmail.com>
2021-12-22 01:22:16 +0300

    feat: [ui] dashboard: создание группы через диалог

--- ui/src/app/pages/dashboard/dashboard-page/dashboard-page.component.ts
+++ ui/src/app/pages/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
-import { DashboardDto, NoteDto } from '@app/models';
+import { DashboardDto, GroupCreateDto, NoteDto } from '@app/models';
 import { GroupFacade } from '@app/store/group';
 import { map } from 'rxjs/operators';
 import { ModalService } from 'am-bulba';
 import { NoteMenuModalComponent } from '@app/shared/components/notes/note-menu-modal/note-menu-modal.component';
+import { CreateGroupModalComponent } from '@app/shared/components/groups/create-group-modal/create-group-modal.component';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -21,8 +22,9 @@ export class DashboardPageComponent implements OnInit {
 
   constructor(
     private facade: GroupFacade,
-    private modalService: ModalService
-  ) { }
+    private modalService: ModalService,
+  ) {
+  }
 
   ngOnInit(): void {
   }
@@ -30,6 +32,22 @@ export class DashboardPageComponent implements OnInit {
   editNote(note: NoteDto) {
     this.modalService.open(NoteMenuModalComponent, note, {
       backgroundClass: 'bg-base-200'
-    })
+    });
+  }
+
+  openCreateGroupModal() {
+    this.modalService.open<string | undefined>(CreateGroupModalComponent, null, {
+      backgroundClass: 'bg-base-200'
+    }).subscribe((title) => {
+      title && this.createGroup(title);
+    });
+  }
+
+  private createGroup(title: string) {
+    const dto: GroupCreateDto = {
+      dashboardId: this.dashboard.id,
+      title,
+    };
+    this.facade.create(dto);
   }
 }
