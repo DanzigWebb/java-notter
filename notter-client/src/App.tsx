5eda142bd83ae1ae4f853afbb7d30edd7d64651c
Alexander <valverin95@gmail.com>
2022-02-19 20:30:08 +0300

    feat: создан основной шаблон

--- notter-client/src/App.tsx
+++ notter-client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Header } from './template';
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <main className="main min-h-screen grid">
+            <Header/>
+            <div>
+                <Outlet/>
+            </div>
+        </main>
+    );
 }
 
 export default App;
---
+++ notter-client/src/template/core/Header.tsx
@@ -0,0 +1,46 @@
+export const Header = () => {
+    return (
+        <header className="navbar bg-base-200">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                        </svg>
+                    </label>
+                    <ul
+                        tabIndex={1}
+                        className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                    >
+                        <li>
+                            <a>Item 1</a>
+                        </li>
+                        <li tabIndex={0}>
+                            <a>Parent</a>
+                        </li>
+                        <li>
+                            <a>Item 3</a>
+                        </li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    <li>
+                        <a>Item 1</a>
+                    </li>
+                    <li tabIndex={0}>
+                        <a>Parent</a>
+                    </li>
+                    <li>
+                        <a>Item 3</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Login</a>
+            </div>
+        </header>
+    );
+};
