2c3c35e3986efb950f8a3ef137a6d9ad1ab8cd2c
Alexander <valverin95@gmail.com>
2022-03-03 19:40:51 +0300

    feat: [solidjs] notFound: увеличен шрифт

--- solidjs/src/pages/not-found/NotFound.tsx
+++ solidjs/src/pages/not-found/NotFound.tsx
@@ -12,7 +12,7 @@ export const NotFound: Component = () => {
             <div class="hero h-full bg-base-200">
                 <div class="text-center hero-content">
                     <div class="max-w-md">
-                        <h1 class="text-5xl font-bold">Hello there</h1>
+                        <h1 class="text-9xl font-bold">404</h1>
                         <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
                             exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
 
