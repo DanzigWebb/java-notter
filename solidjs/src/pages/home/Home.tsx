7b9bbb30880de64785be113758bc798b925609fe
Alexander <valverin95@gmail.com>
2022-03-08 01:31:46 +0300

    feat: [solidjs] home: создана карточка стартового экрана

--- solidjs/src/pages/home/Home.tsx
+++ solidjs/src/pages/home/Home.tsx
@@ -1,10 +1,51 @@
-import { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 import { Page } from '@root/src/pages/Page';
+import { FormField } from '@components/form/group/FormField';
 
 export const Home: Component = (props) => {
+
+    const [name, setName] = createSignal('');
+
+    function onSubmit() {
+
+    }
+
     return (
         <Page>
-            <h2>Home</h2>
+            <div className="container py-6 m-4">
+                <div class="card shadow-xl bg-base-200 text-center max-w-sm mx-auto">
+                    <figure>
+                        <img class="w-full"
+                             src="https://a.trellocdn.com/prgb/dist/images/home/orientation/new-user.e8544e0e1b2824e4ac46.svg"
+                             alt=""/>
+                    </figure>
+
+                    <div className="card-body">
+                        <div class="opacity-90 mb-4">
+                            <h3 class="text-xl font-semibold pb-2">Организуйте что угодно</h3>
+                            <p class="text-sm opacity-80">Соберите все в одном месте и начни перемещать элементы на
+                                вашей первой доске!</p>
+                        </div>
+
+                        <FormField>
+                            <input
+                                type="text"
+                                class="input mb-2"
+                                placeholder="Над чем вы работаете?"
+                                onInput={e => setName((e.target as HTMLInputElement).value)}
+                            />
+                        </FormField>
+
+                        <button
+                            className="btn btn-primary text-xs"
+                            disabled={name().length === 0}
+                            onClick={onSubmit}
+                        >
+                            Создайте свою доску
+                        </button>
+                    </div>
+                </div>
+            </div>
         </Page>
-    )
-}
+    );
+};
